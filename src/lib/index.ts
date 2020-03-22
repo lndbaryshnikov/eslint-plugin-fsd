63163b29d1519d7990d3bb70dabfafb3e0c3ef45
Roman Korenev <romengrus@gmail.com>
2020-03-22 17:46:54 +0200

    Add sharable config
    User can use this config with
    extends: ['plugin:fsd/all']

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,6 +4,7 @@
  */
 
 import HOFNamePrefixRule from './rules/hof-name-prefix';
+import NoHeavyConstructor from './rules/no-heavy-constructor';
 import { rule as SplitConditionals } from './rules/split-conditionals';
 import { rule as JqCacheDOMElements } from './rules/jq-cache-dom-elements';
 import { rule as JqUseJSPrefixInSelector } from './rules/jq-use-js-prefix-in-selector';
@@ -16,5 +17,19 @@ module.exports = {
     'jq-cache-dome-elements': JqCacheDOMElements,
     'jq-use-js-prefix-in-selector': JqUseJSPrefixInSelector,
     'no-function-declaration-in-event-listener': NoFunctionDeclarationInEventListener,
+    'no-heavy-constructor': NoHeavyConstructor,
+  },
+  configs: {
+    all: {
+      plugins: ['fsd'],
+      rules: {
+        'fsd/hof-name-prefix': 2,
+        'fsd/no-heavy-constructor': 2,
+        'fsd/jq-cache-dome-elements': 2,
+        'fsd/jq-use-js-prefix-in-selector': 2,
+        'fsd/no-function-declaration-in-event-listener': 2,
+        'fsd/split-conditionals': 2,
+      },
+    },
   },
 };
