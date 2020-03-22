13f5bd1f5a4a114b151e913c3998aa152dcc3a35
Roman Korenev <romengrus@gmail.com>
2020-03-22 14:11:06 +0200

    Add rules to plugin index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,9 +4,17 @@
  */
 
 import HOFNamePrefixRule from './rules/hof-name-prefix';
+import { rule as SplitConditionals } from './rules/split-conditionals';
+import { rule as JqCacheDOMElements } from './rules/jq-cache-dom-elements';
+import { rule as JqUseJSPrefixInSelector } from './rules/jq-use-js-prefix-in-selector';
+import { rule as NoFunctionDeclarationInEventListener } from './rules/no-function-declaration-in-event-listener';
 
 module.exports = {
   rules: {
     'hof-name-prefix': HOFNamePrefixRule,
+    'split-conditionals': SplitConditionals,
+    'jq-cache-dome-elements': JqCacheDOMElements,
+    'jq-use-js-prefix-in-selector': JqUseJSPrefixInSelector,
+    'no-function-declaration-in-event-listener': NoFunctionDeclarationInEventListener,
   },
 };
