8f3eaf87c461b8e2afb3956b7239509115288da7
Roman Korenev <romengrus@gmail.com>
2020-03-25 01:31:18 +0200

    Manually add rules to index instead of using requireindex
    Reason: Error is raised during plugin usage (unable to find rules)

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,7 +4,34 @@
  */
 
 import 'core-js/stable';
-import requireIndex from 'requireindex';
 
-// import all rules in lib/rules
-module.exports.rules = requireIndex(`${__dirname}/rules`);
+import HofNamePrefix from './rules/hof-name-prefix';
+import NoHeavyConstructor from './rules/no-heavy-constructor';
+import { rule as JQCacheDOMElements } from './rules/jq-cache-dom-elements';
+import { rule as JQUseJSPrefixInSelector } from './rules/jq-use-js-prefix-in-selector';
+import { rule as NoFunctionDeclarationInEventListener } from './rules/no-function-declaration-in-event-listener';
+import { rule as SplitConditionals } from './rules/split-conditionals';
+
+module.exports = {
+  rules: {
+    'hof-name-prefix': HofNamePrefix,
+    'no-heavy-constructor': NoHeavyConstructor,
+    'jq-cache-dom-elements': JQCacheDOMElements,
+    'jq-use-js-prefix-in-selector': JQUseJSPrefixInSelector,
+    'no-function-declaration-in-event-listener': NoFunctionDeclarationInEventListener,
+    'split-conditionals': SplitConditionals,
+  },
+  configs: {
+    all: {
+      plugins: ['fsd'],
+      rules: {
+        'fsd/hof-name-prefix': 'error',
+        'fsd/no-heavy-constructor': 'error',
+        'fsd/jq-cache-dom-elements': 'error',
+        'fsd/jq-use-js-prefix-in-selector': 'error',
+        'fsd/no-function-declaration-in-event-listener': 'error',
+        'fsd/split-conditionals': 'error',
+      },
+    },
+  },
+};
